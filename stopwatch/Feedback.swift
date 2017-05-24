1d118ee83aac553727e5de1f95df9b9f0f081772
Juxhin (Eugene) Bakalli <sofokli1@gmail.com>
2017-05-24 19:42:45 +0300

    :construction: Split models to different files

---
+++ stopwatch/Feedback.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Feedback
+{
+    let placeholderText: String
+    let negativeButtonText: String
+    let positiveButtonText: String
+}
--- stopwatch/Question.swift
+++ stopwatch/Question.swift
@@ -6,10 +6,3 @@ struct Question
     let negativeButtonText: String
     let positiveButtonText: String
 }
-
-struct Feedback
-{
-    let placeholderText: String
-    let negativeButtonText: String
-    let positiveButtonText: String
-}
